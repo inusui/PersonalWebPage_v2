6e50b7836dd0e1f5c40091ed5e83f3ea9e0eaeb7
Ricardo Domínguez González <radg21@outlook.es>
2022-11-30 20:37:33 -0500

    esqueleto de los componentes, añadido de primeng, primeicon, cambio del favicon Añadido de los iconos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { SocialBarComponent } from './pages/social-bar/social-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BlogComponent,
+    SocialBarComponent
   ],
   imports: [
     BrowserModule
