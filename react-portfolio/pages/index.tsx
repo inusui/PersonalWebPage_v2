40e7ffce39e81e9124510396c0cfbcdd156ac055
Ricardo Domínguez González <radg21@outlook.es>
2023-01-17 22:04:53 -0500

    Añadiendo bg y usando react icons - 17:10

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Roboto_Mono } from '@next/font/google'
+import{BsFillMoonStarsFill} from 'react-icons/bs'
 
 const inter = Roboto_Mono({ subsets: ['latin'] })
 
@@ -12,9 +13,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className=' bg-indigo-900 min-h-screen' >
-          <h1>si, Otra version de mi portafolio xd </h1>
+      <main className='bg-blue px-10'>
+        <section className='min-h-screen' >
+          <div className='py-10 mb-12 flex justify-between'>
+            <h1>developed byed</h1>
+            <ul>
+              Quede en 17:10
+              <li> <BsFillMoonStarsFill/> </li>
+              <li><a href="$">Resume</a></li>
+            </ul>
+          </div>
         </section>
       </main>
 
