73537a0015fd3f565dbb412c084ea0c27138d24a
Ricardo Domínguez González <radg21@outlook.es>
2023-01-22 01:17:14 -0500

    SEccion 1 terminada

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import { Roboto_Mono } from '@next/font/google';
 import { BsFillMoonStarsFill, BsReddit } from 'react-icons/bs';
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png"
 
 const inter = Roboto_Mono({ subsets: ['latin'] })
 
@@ -27,24 +29,38 @@ export default function Home() {
             </ul>
             quede en 22:30
           </nav>
-          <div className="text-center p-10">
+          <div className="text-center p-12">
             <h2 className="text-4xl py-2 text-teal-600 font-medium" >Ricardo</h2>
             <h3 className="text-2xl py-2">Developer</h3>
             <hr />
-            <p className="text-md py-5 leading-8 text-gray-800">
-              Frontend: PhP, JavaScript usando frameworks como ejs, angular, react. <br />
+            <p className="text-justify text-md py-5 leading-8 text-gray-800">
+             Desarrollador que provee de sus servicios en el contenido tecnológico que necesites 
+             </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-10 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillGithub />
+            <BsReddit />
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image src={deved} alt='Not Image Found' layout='fill' objectFit='cover' />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Services I Offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+            Frontend: PhP, JavaScript usando frameworks como ejs, angular, react. <br />
               Backend: Java, Python
               Desarrollo de Apps Móviles con Kotlin, Ciber Seguridad, Python.
+            
             </p>
+Quede en 31:34
           </div>
-            <div className="text-5xl flex justify-center gap-10 py-3 text-gray-600">
-                <AiFillTwitterCircle/>
-                <AiFillLinkedin/>
-                <AiFillGithub/>
-                <BsReddit/>
-            </div>
-            <div></div>
         </section>
+
       </main>
 
 
