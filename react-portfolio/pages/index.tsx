f6eabd3894c075167b930497d6fd1dee139e36d0
Ricardo Domínguez González <radg21@outlook.es>
2023-01-22 23:25:01 -0500

    Add apps bar

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -3,7 +3,6 @@ import { Roboto_Mono } from '@next/font/google';
 import { BsFillMoonStarsFill, BsReddit } from 'react-icons/bs';
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png"
 
 const inter = Roboto_Mono({ subsets: ['latin'] })
 
@@ -34,34 +33,76 @@ export default function Home() {
             <h3 className="text-2xl py-2">Developer</h3>
             <hr />
             <p className="text-justify text-md py-5 leading-8 text-gray-800">
-             Desarrollador que provee de sus servicios en el contenido tecnológico que necesites 
-             </p>
+              Desarrollador que provee de sus servicios en el contenido tecnológico que necesites
+            </p>
           </div>
           <div className="text-5xl flex justify-center gap-10 py-3 text-gray-600">
             <AiFillTwitterCircle />
             <AiFillLinkedin />
             <AiFillGithub />
             <BsReddit />
-          </div>
-          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
-            <Image src={deved} alt='Not Image Found' layout='fill' objectFit='cover' />
-          </div>
-        </section>
+          </div>         
+      </section>
 
-        <section>
-          <div>
-            <h3 className='text-3xl py-1'>Services I Offer</h3>
-            <p className='text-md py-2 leading-8 text-gray-800'>
+      <section>
+        <div>
+          <h3 className='text-3xl py-1'>Services I Offer</h3>
+          <hr />
+          <p className='text-md py-2 leading-8 text-gray-800'>
             Frontend: PhP, JavaScript usando frameworks como ejs, angular, react. <br />
-              Backend: Java, Python
-              Desarrollo de Apps Móviles con Kotlin, Ciber Seguridad, Python.
-            
-            </p>
-Quede en 31:34
-          </div>
-        </section>
+            Backend: Java, Python
+            Desarrollo de Apps Móviles con Kotlin, Ciber Seguridad, Python.
 
-      </main>
+          </p>
+          Quede en 31:34
+        </div>
+        <div className='text-5xl py-3 flex justify-center gap-2'>
+            <a href="https://developer.android.com" target="_blank" rel="noreferrer">
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40" /> </a>
+            <a href="https://angular.io" target="_blank" rel="noreferrer">
+              <Image src="https://angular.io/assets/images/logos/angular/angular.svg" alt="angular" width="40" height="40" /> </a>
+            <a href="https://couchdb.apache.org/" target="_blank" rel="noreferrer">
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/0d6c64dbbf311879f7d563bfc3ccf559f9ed111c/icons/couchdb/couchdb-original.svg" alt="couchdb" width="40" height="40" />
+            </a>
+            <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40" />
+            </a>
+            <a href="https://firebase.google.com/" target="_blank" rel="noreferrer">
+              <Image src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40" />
+            </a>
+            <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer">
+              <Image src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width="40" height="40" />
+            </a>
+            <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+              <Image src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" />
+            </a>
+            
+        </div>
+        <div className='flex justify-center gap-2'>
+        <a href="https://kotlinlang.org" target="_blank" rel="noreferrer">
+              <Image src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40" /> 
+            </a>
+            <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> 
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" /> 
+            </a> 
+            <a href="https://www.microsoft.com/en-us/sql-server" target="_blank" rel="noreferrer"> 
+              <Image src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg" alt="mssql" width="40" height="40" /> 
+            </a> 
+            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> 
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40" /> 
+            </a> 
+            <a href="https://nodejs.org" target="_blank" rel="noreferrer"> 
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" /> 
+            </a>
+            <a href="https://www.php.net" target="_blank" rel="noreferrer"> 
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40" /> 
+            </a>
+            <a href="https://www.python.org" target="_blank" rel="noreferrer"> 
+              <Image src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40" /> 
+            </a>
+        </div>
+      </section>
+
+    </main>
 
 
     </>
