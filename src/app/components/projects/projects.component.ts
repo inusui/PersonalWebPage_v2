e15210f82b90ba3f30002fe3e582a0b37856274a
Inusui <radg21@outlook.es>
2024-06-27 16:00:45 -0500

    projects component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { DataService } from '../../services/data-service.service';
 import { PrimeNgModule } from '../../common/PrimeNgModule';
+import { PanelModule } from 'primeng/panel';
+import { DataViewModule } from 'primeng/dataview';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CarouselModule, PrimeNgModule],
+  imports: [CarouselModule, PrimeNgModule, PanelModule, DataViewModule, RouterModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
