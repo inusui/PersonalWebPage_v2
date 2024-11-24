d88a3bb70f6ce979fa54cf59ff75a32aed10178d
inusui <radg21@outlook.es>
2024-11-23 22:12:34 -0500

    feat(icon): se añadio paquete de iconos de pictogramer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { BannerComponent } from './components/banner/banner.component';
 import { PrimeNgModule } from './common/PrimeNgModule';
+import { BannerComponent } from './components/banner/banner.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { LastPublicationsComponent } from './components/last-publications/last-publications.component';
 import { DataService } from './services/data-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    BannerComponent,
-    PrimeNgModule,
-    ProjectsComponent
-],
+  imports: [BannerComponent, PrimeNgModule, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-
   products: any;
   responsiveOptions: any;
 
   constructor(private dataService: DataService) {}
 
-
   ngOnInit(): void {
-    
     this.dataService.getProjects().subscribe((data) => {
       this.products = data;
     });
